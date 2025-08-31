233e5be7e629acae1e3b71b4f773778596f34880
Mahir123456789123 <165038073+Mahir123456789123@users.noreply.github.com>
2025-08-31 18:06:16 +0530

    configuring config.py

--- flask/config.py
+++ flask/config.py
@@ -1,7 +1,7 @@
 import os
 
 # Supabase settings - Use environment variables for production
-SUPABASE_URL = os.environ.get("SUPABASE_URL", "https://opvaodgzjzdrpiqqrwuk.supabase.co")
+SUPABASE_URL = os.environ.get("SUPABASE_URL", "https://opvaodgzjzdrpiqqrwuk.supabase.co/rest/v1")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wdmFvZGd6anpkcnBpcXFyd3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY2NDE5OTIsImV4cCI6MjA3MjIxNzk5Mn0.bj2XCXC8nQzcFNYwHQyus7BcTy9xnVA-S573bYu2xXE")
 
 SUPABASE_HEADERS = {
