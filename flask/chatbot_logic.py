55b62f643376cac70df4293080cb511570e6caf7
Mahir123456789123 <165038073+Mahir123456789123@users.noreply.github.com>
2025-08-31 15:45:33 +0530

    gemini key1

--- flask/chatbot_logic.py
+++ flask/chatbot_logic.py
@@ -2,7 +2,7 @@
 import google.generativeai as genai
 import requests
 
-genai.configure(api_key="AIzaSyCxFXT_4QYc7Dp1AEPnSmbqJYDGXAN-A6Y")
+genai.configure(api_key="AIzaSyASik60ziPHwdQLH1OP_H2m24IHLbwiFi0")
 model = genai.GenerativeModel(model_name="gemini-1.5-pro")
 
 BACKEND_API = "http://127.0.0.1:5000"
